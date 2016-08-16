918f80dba7575d663d3208584497e2a7a7618ea2
YK Song <YK Song>
2016-08-16 12:11:37 +0800

    class

--- bilibiliCoverCrawler.py
+++ bilibiliCoverCrawler.py
@@ -4,53 +4,61 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-#得到视频所在链接
-def inputAVnum(url):
-    avmun_pattern = re.compile(r'(av)?\d+')
-    while True:
-        avnum = input();
-        avnum = avmun_pattern.search(avnum)
-        if avnum:
-            avnum = avnum.group()
-            if avnum[0] != 'a':
-                avnum = 'av' + avnum
-            break
+class Crawler(object):
+    searchURL = 'http://search.bilibili.com/all?keyword='
+    img = None
+    retImg = None
+
+    def start(self):
+        self.inputAVnum()
+        self.findImg()
+        self.getOriginImg()
+
+    #得到视频所在链接
+    def inputAVnum(self):
+        avmun_pattern = re.compile(r'(av)?\d+')
+        while True:
+            avnum = input();
+            avnum = avmun_pattern.search(avnum)
+            if avnum:
+                avnum = avnum.group()
+                if avnum[0] != 'a':
+                    avnum = 'av' + avnum
+                break
+            else:
+                print('请输入正确AV号或视频链接，如:av5685228或5685228')
+        self.searchURL = self.searchURL + avnum
+
+    #得到封面缩略图链接
+    def findImg(self):
+        wb_data = requests.get(self.searchURL)
+        soup = BeautifulSoup(wb_data.text, 'lxml')
+        try:
+            imgs = soup.select('div.so-wrap > ul > li > a > div > img')
+            self.img = imgs[0].get('src')
+        except:
+            print('该视频不存在')
+
+    #处理缩略图链接，得到原图链接
+    def getOriginImg(self):
+        imgurl_pattern_new = re.compile(r'(.+)(.jpg_\d{3}x\d{3})(.jpg)')
+        imgurl_pattern_old = re.compile(r'(.+)(/\d{3}_\d{3})(.+)')
+        if re.match(imgurl_pattern_new, self.img):
+            imgurl = re.match(imgurl_pattern_new, self.img)   
+        elif re.match(imgurl_pattern_old, img):
+            imgurl = re.match(imgurl_pattern_old, self.img) 
         else:
-            print('请输入正确AV号或视频链接，如:av5685228或5685228')
-    url = url + avnum
-    return url
+            print('error')
+        self.retImg = imgurl.group(1)+imgurl.group(3)
 
-#得到封面缩略图链接
-def findImg(url):
-    wb_data = requests.get(url)
-    soup = BeautifulSoup(wb_data.text, 'lxml')
-    try:
-        imgs = soup.select('div.so-wrap > ul > li > a > div > img')
-        img = imgs[0].get('src')
-    except:
-        print('该视频不存在')
-    return img
-
-#处理缩略图链接，得到原图链接
-def getOriginImg(img):
-    imgurl_pattern_new = re.compile(r'(.+)(.jpg_\d{3}x\d{3})(.jpg)')
-    imgurl_pattern_old = re.compile(r'(.+)(/\d{3}_\d{3})(.+)')
-    if re.match(imgurl_pattern_new, img):
-        imgurl = re.match(imgurl_pattern_new, img)   
-    elif re.match(imgurl_pattern_old, img):
-        imgurl = re.match(imgurl_pattern_old, img) 
-    else:
-        print('error')
-    return (imgurl.group(1)+imgurl.group(3))
+    def getRetURL(self):
+        return self.retImg
 
 if __name__ == '__main__':
-    searchURL = 'http://search.bilibili.com/all?keyword='
-    viodeURL = inputAVnum(searchURL)
-    
-    img = findImg(viodeURL)
-    imgurl = getOriginImg(img)
-    print(imgurl)
-    
+    crawler = Crawler()
+    crawler.start()
+    print(crawler.getRetURL())
+
 #两种封面链接形式,2016年2月后的视频为 new ,之前为 old
 ''' 
 new = http://i0.hdslb.com/bfs/archive/eaa41481620de4f4126cb502cd9ef16a51fa6ecf.jpg_320x200.jpg
